0229dbdd16eec47a8c2f8b7d66e4d47d7323009b
Cleriston Martins Cardoso <cleriston.mari@gmail.com>
2026-04-21 17:07:53 -0300

    feat: use feature remove of lists

--- streams/lists/src/main/java/org/example/App.java
+++ streams/lists/src/main/java/org/example/App.java
@@ -13,6 +13,19 @@ public class App
         nomes.add("Marina");
         nomes.add("Martina");
 
+        System.out.println("Primeiro nome: " + nomes.get(0));
+
+        for (int i = 0; i < nomes.size(); i++) {
+            System.out.println("Index " + i + ": " + nomes.get(i));
+        }
+
+        for (String nome : nomes) {
+            System.out.println("Nome: " + nome);
+        }
+
+        nomes.forEach(nome -> System.out.println("Lambda: " + nome));
+
+        nomes.remove("Martina");
 
         System.out.println("Tamanho: " + nomes.size());
     }

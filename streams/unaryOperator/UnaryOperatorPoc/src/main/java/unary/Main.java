6a24728f5919a058139cbbf186cce577ca6784aa
Cleriston Martins Cardoso <cleriston.mari@gmail.com>
2026-06-05 23:54:56 -0300

    feat: add UnaryOperator andThen method

--- streams/unaryOperator/UnaryOperatorPoc/src/main/java/unary/Main.java
+++ streams/unaryOperator/UnaryOperatorPoc/src/main/java/unary/Main.java
@@ -4,12 +4,17 @@ package unary;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 
 public class Main {
     static void main() {
         UnaryOperator<Double> calcSalerBonus = orderValue -> (orderValue * 10) / 100;
-        System.out.println("Saler Bonud: " + calcSalerBonus.apply(1000.00));
+        UnaryOperator<Double> calcSalerBonusVat = bonus -> (bonus * 4) / 100;
+        Function<Double, Double> result = calcSalerBonus.andThen(calcSalerBonusVat);
+
+        System.out.println("Saler Bonus: " + calcSalerBonus.apply(1000.00));
+        System.out.println("Saler VAT: " + result.apply(1000.00));
     }
 }
